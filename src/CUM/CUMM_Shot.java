fabb4230644bef495fff6dcd6be99fc061ad6380
TheL0st <79166172444@yandex.ru>
2021-12-14 20:40:47 +0300

    SkAM

--- src/CUM/CUMM_Shot.java
+++ src/CUM/CUMM_Shot.java
@@ -7,21 +7,39 @@ public class CUMM_Shot
 {
     public static File pic1 = new File("E:\\desktop\\drawing content\\");
     public static void main(String[] args)
-
     {
         List <String> files = new ArrayList<>();
         List <String> dirs = new ArrayList<>();
         List <String> files_path = new ArrayList<>();
         String path1 = pic1.getAbsolutePath();
-        readFiles(pic1,dirs,files);
+        int count = readFiles(pic1,dirs,files,true);
+
+        repeat(pic1, dirs,files_path,files);
+        yourFilesShallBeMINE(dirs, files_path, files);
+
         System.out.println(files);
+        System.out.println(count);
         System.out.println(dirs);
-        yourFilesShallBeMINE(dirs,files_path,files);
-        yourFilesShallBeMINE(dirs,files_path,files);
+        System.out.println(files_path);
 
     }
 
-    public static void readFiles(File baseDirectory, List dirs,List files){
+    public static void repeat(File baseDirectory, List dirs, List files_path, List files)
+    {
+        String temp1 = baseDirectory.getAbsolutePath() + "en";
+        String temp2 = baseDirectory.getAbsolutePath() + "ru";
+        File porn2 = new File(temp1);
+        File porn3 = new File(temp2);
+        int temp_ru = readFiles(porn2, dirs,files,false);
+        int temp_en = readFiles(porn3, dirs,files,false);
+        System.out.println(temp_ru);
+        System.out.println(temp_en);
+    }
+
+
+
+    public static int readFiles(File baseDirectory, List dirs,List files, boolean what){
+        int counter = 0;
         if (baseDirectory.isDirectory()){
             for (File file : baseDirectory.listFiles())
             {
@@ -31,11 +49,16 @@ public class CUMM_Shot
                 }
                 else
                     {
-                    dirs.add(file.getName());
-                    readFiles(file,dirs,files);
+                        if (what)
+                        {
+                            dirs.add(file.getName());
+                        }
+                    counter++;
+                    readFiles(file,dirs,files, what);
                     }
                 }
             }
+        return counter;
         }
 
     public static void readFiles1(File baseDirectory, List files_path){
@@ -59,10 +82,10 @@ public class CUMM_Shot
                 temp += "\\" + dirs.get(0);
                 dirs.remove(0);
                 File porn1 = new File(temp);
-                System.out.println(temp);
-                System.out.println(dirs);
+                //System.out.println(temp);
+               // System.out.println(dirs);
                 readFiles1(porn1, files_path);
-                System.out.println(files_path);
+                //System.out.println(files_path);
             }
         }
     }
